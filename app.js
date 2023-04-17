5e0719afb56f5db8327411aedc47905d3f1ada4b
dipikarajesh18 <dipika.rajesh@gmail.com>
2023-04-17 17:31:57 -0400

    add start script

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/style.css', (req, res) => {
 // define route for fetching a random sprite from the sprite_labels table
 app.get('/get-random-sprite', (req, res) => {
   // select a random sprite from the sprite_labels table
-  const query = 'SELECT * FROM sprite_labels ORDER BY RAND() LIMIT 1';
+  const query = 'SELECT * FROM sprite_labels WHERE user_label IS NULL AND is_unknown IS NULL ORDER BY RAND() LIMIT 1';
   sql.query(query, (err, results) => {
     if (err) {
       throw err;
@@ -37,28 +37,26 @@ app.get('/get-random-sprite', (req, res) => {
   });
 });
 
-// // Handle POST request to insert an annotation
-// app.post('/insert-annotation', (req, res) => {
-//   // Get the data from the POST request
-//   console.log(req.body)
-//   const spriteId = req.body.sprite_id;
-//   const annotation = req.body.annotation;
-//   const ascii_map = req.body.ascii_map
-//   const map_size = req.body.map_size
-//   const username = req.body.uname
-//   console.log("INSERTING:")
-//   console.log("Level ID:", levelId);
-//   console.log("ASCII_MAP:", ascii_map);
-//   console.log("MAP_SIZE:", map_size);
-//   console.log("ANNOTATION:", annotation);
+// Handle POST request to insert an annotation
+app.post('/insert-annotation', (req, res) => {
+  // Get the data from the POST request
+  console.log(req.body)
+  const spriteId = req.body.sprite_id;
+  const annotation = req.body.annotation;
+  const sprite_hex = req.body.sprite_hex;
+  const username = req.body.uname
+  console.log("INSERTING:")
+  console.log("SPRITE DB ID:", spriteId);
+  console.log("SPRITE_HEX:", sprite_hex);
+  console.log("ANNOTATION:", annotation);
   
-//   // Insert the annotation into the database
-//   const query = 'INSERT INTO user_levels_annotated (ANNOTATION_ID, LEVEL_ID, ASCII_MAP, MAP_SIZE, TIME_MADE, ANNOTATION, ANN_AUTHOR) VALUES (null, ?, ?, ?, ?, ?, ?)';
-//   sql.query(query, [levelId, ascii_map, map_size, 'CURRENT_TIMESTAMP', annotation, username], (err, result) => {
-//     if (err) {
-//       throw err;
-//     }
-//     // Send a success message as a JSON object
-//     res.json({ message: 'Annotation inserted successfully' });
-//   });
-// })
+  // Insert the annotation into the database
+  const query = 'INSERT INTO user_levels_annotated (ANNOTATION_ID, LEVEL_ID, ASCII_MAP, MAP_SIZE, TIME_MADE, ANNOTATION, ANN_AUTHOR) VALUES (null, ?, ?, ?, ?, ?, ?)';
+  sql.query(query, [levelId, ascii_map, map_size, 'CURRENT_TIMESTAMP', annotation, username], (err, result) => {
+    if (err) {
+      throw err;
+    }
+    // Send a success message as a JSON object
+    res.json({ message: 'Annotation inserted successfully' });
+  });
+})
