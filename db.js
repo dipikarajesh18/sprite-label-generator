d612d3874152841c9ec0acf492973f1d5a11f635
Dipika Rajesh <dipika.rajesh@gmail.com>
2023-11-27 21:30:20 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const connection = mysql.createPool({
-    host: 'us-cdbr-east-06.cleardb.net',
-    user: 'b50281fd4181c6',
-    password: '12b4a1bb',
-    database: 'heroku_2f2d25ae5fc707a'
+    host: 'host',
+    user: 'user',
+    password: 'password',
+    database: 'database'
 });
 
 module.exports = connection;
